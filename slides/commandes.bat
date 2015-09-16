504168d3482c4d3747094c6bf117421ee5769d61
Arnaud Tournier <tourniera.a-ssii@mipih.fr>
2015-09-16 09:42:04 +0200

    avant revision

--- slides/commandes.bat
+++ slides/commandes.bat
@@ -1,3 +1,5 @@
 rem pandoc -s -t revealjs annotation-processing.md -o annotation-processing.html --css reveal.js/css/theme/beige.css --slide-level 3
 
 pandoc -s -t revealjs annotation-processing.md -o annotation-processing.html --css reveal.js/css/theme/beige.css --slide-level 3 --no-highlight --variable hlss=zenburn --template reveal-template.html
+
+pandoc -s -t revealjs annotation-processing-light.md -o annotation-processing-light.html --css reveal.js/css/theme/beige.css --slide-level 3 --no-highlight --variable hlss=zenburn --template reveal-template.html
